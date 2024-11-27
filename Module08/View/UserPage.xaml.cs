a2e9e6694e0c07e79da5a44668b1a626624ef659
CristanJosh <cristanjosh03@gmail.com>
2024-11-27 09:43:42 +0800

    Add project files.

---
+++ Module08/View/UserPage.xaml.cs
@@ -0,0 +1,11 @@
+using Module08.ViewModel;
+namespace Module08.View;
+
+public partial class UserPage : ContentPage
+{
+	public UserPage()
+	{
+		InitializeComponent();
+		BindingContext = new UserViewModel();
+	}
+}
---
+++ Module08/ViewModel/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module08.Model;
+using Module08.Services;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Module08.ViewModel
+{
+    public class UserViewModel : BindableObject
+    {
+        private readonly UserService _userService;
+        public ObservableCollection<User> Users { get; private set; }
+        public UserViewModel() 
+        { 
+            _userService = new UserService();
+            Users = new ObservableCollection<User>();
+            LoadUserCommand = new Command(async () => await LoadUsers());
+        }
+
+        public ICommand LoadUserCommand { get; }
+
+        private async Task LoadUsers()
+        {
+            var users = await _userService.GetUserAsync();
+            Users.Clear();
+            foreach (var user in users)
+            {
+                Users.Add(user);
+            }
+        }
+    }
+}
